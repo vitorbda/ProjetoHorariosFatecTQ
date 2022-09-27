a9b3e36084200a1584676d62d2c7c9312f272e70
vitorbda <80553392+vitorbda@users.noreply.github.com>
2022-09-27 09:51:10 -0300

    Inicio do projeto

---
+++ ProjetoHorariosFatec/ProjetoHorariosFatec/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoHorariosFatec.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
